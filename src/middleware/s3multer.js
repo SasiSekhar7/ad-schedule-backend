609ea79d568cbc331e4f7b2b104a36a0d039b850
SasiSekhar7 <73186341+SasiSekhar7@users.noreply.github.com>
2025-10-19 17:05:33 +0530

    modified max file size for now

--- src/middleware/s3multer.js
+++ src/middleware/s3multer.js
@@ -31,7 +31,7 @@ const generalMediaStorage = multer.diskStorage({
 // Set up Multer for disk storage for general uploads
 const upload = multer({
     storage: generalMediaStorage, // Use the new diskStorage configuration
-    limits: { fileSize: 250 * 1024 * 1024 }, // Increased limit for general media, e.g., 250MB
+    limits: { fileSize: 400 * 1024 * 1024 }, // Increased limit for general media, e.g., 250MB
     fileFilter: (req, file, cb) => {
         // Example: Only allow images and videos for general media upload
         if (file.mimetype.startsWith('image/') || file.mimetype.startsWith('video/')) {
