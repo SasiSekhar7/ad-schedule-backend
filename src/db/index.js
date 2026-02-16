dd8fb12c8ceebc482b1c1bee0e1100f8c9f7403b
Navdeep Pathan <151203532+navdeeppathan@users.noreply.github.com>
2026-02-16 18:42:17 +0530

    navdeep_changes

--- src/db/index.js
+++ src/db/index.js
@@ -11,9 +11,9 @@ const config = {
   dialect: process.env.DB_DIALECT || "postgres",
   host: process.env.DB_HOST || "localhost",
   port: process.env.DB_PORT || 5432,
-  username: process.env.DB_USER || "adupuser",
-  password: process.env.DB_PASSWORD || "Birla@1122",
-  database: process.env.DB_NAME || "testdb",
+  username: process.env.DB_USER || "postgres",
+  password: process.env.DB_PASSWORD || "root",
+  database: process.env.DB_NAME || "consoledb",
   logging: false, // optional
 };
 
@@ -24,7 +24,7 @@ const sequelize = new Sequelize(
   config.database,
   config.username,
   config.password,
-  config
+  config,
 );
 
 module.exports = sequelize;
