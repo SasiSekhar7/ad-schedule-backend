e756fca5b8bec697329c44d925bca83071de1c4e
Navdeep Pathan <151203532+navdeeppathan@users.noreply.github.com>
2026-02-20 16:18:42 +0530

    20022026

--- src/db/index.js
+++ src/db/index.js
@@ -11,9 +11,9 @@ const config = {
   dialect: process.env.DB_DIALECT || "postgres",
   host: process.env.DB_HOST || "localhost",
   port: process.env.DB_PORT || 5432,
-  username: process.env.DB_USER || "postgres",
-  password: process.env.DB_PASSWORD || "root",
-  database: process.env.DB_NAME || "testconsoledb",
+  user: process.env.DB_USER || "adupuser",
+  password: process.env.DB_PASSWORD || "Birla@1122",
+  database: process.env.DB_NAME || "testdb",
   logging: false, // optional
 };
 
--- src/db/migrateSqliteToPostgres.js
+++ src/db/migrateSqliteToPostgres.js
@@ -8,12 +8,10 @@ const sqliteDb = new sqlite3.Database(
 const pgClient = new Client({
   host: process.env.DB_HOST || "localhost",
   port: process.env.DB_PORT || 5432,
-  // user: process.env.DB_USER || "adupuser",
-  // password: process.env.DB_PASSWORD || "Birla@1122",
-  // database: process.env.DB_NAME || "testdb",
-  user: process.env.DB_USER || "postgres",
-  password: process.env.DB_PASSWORD || "root",
-  database: process.env.DB_NAME || "testconsoledb",
+  user: process.env.DB_USER || "adupuser",
+  password: process.env.DB_PASSWORD || "Birla@1122",
+  database: process.env.DB_NAME || "testdb",
+
   logging: false, // optional
 });
 async function migrateTable(tableName, columns) {
